747baeef4a6627d59bfe7b4cc5c1c99afd91dc51
Vitaly Turovsky <vital2580@icloud.com>
2022-01-18 12:17:29 +0300

    we no longer live there

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,20 @@
-import { extensionCtx } from 'vscode-framework'
+import vscode from 'vscode'
+import 'vscode-framework'
+import { getExtensionSetting } from 'vscode-framework'
 
 export const activate = () => {
     // TODO add web check
-    // extensionCtx.
+    // TODO setting change
+    if (process.env.PLATFORM === 'web' && !getExtensionSetting('web.enable')) return
+    const web = getExtensionSetting('web.enable')
+    vscode.window.onDidChangeWindowState(async ({ focused }) => {
+        if (!focused) return
+        console.time('get setting')
+        console.log(getExtensionSetting('web.enable'))
+        console.timeEnd('get setting')
+        const clipboardText = await vscode.env.clipboard.readText()
+        if (!clipboardText.includes('\\')) return
+        const newPath = clipboardText.split('\\').join('/')
+        await vscode.env.clipboard.writeText(newPath)
+    })
 }
