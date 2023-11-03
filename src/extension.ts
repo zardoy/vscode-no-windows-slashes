d0ec90cd6a2e562482fd3af76c74af0650d4b169
Vitaly Turovsky <vital2580@icloud.com>
2023-11-03 08:24:08 +0300

    implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,19 @@
-import { extensionCtx } from 'vscode-framework'
+import * as vscode from 'vscode'
+import { getExtensionSetting, registerExtensionCommand } from 'vscode-framework'
 
 export const activate = () => {
-    // TODO add web check
-    // extensionCtx.
+    const normalizeClipboard = async () => {
+        if (!getExtensionSetting('enable')) return
+        const clipboard = await vscode.env.clipboard.readText()
+        const patched = clipboard.replaceAll('\\', '/')
+        await vscode.env.clipboard.writeText(patched)
+    }
+
+    registerExtensionCommand('fixedCopyFilePath', async () => {
+        await vscode.commands.executeCommand('copyFilePath')
+        await normalizeClipboard()
+    })
+    vscode.window.onDidChangeWindowState(({ focused }) => {
+        if (focused) normalizeClipboard()
+    })
 }
